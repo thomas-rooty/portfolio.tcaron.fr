e7ca1121a70bf39ad0e6b4dd0338c1653174dbcf
thomas-rooty <essainono1@gmail.com>
2022-02-02 11:24:16 +0100

    added bike

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 
 // Import components
 import Header from "./Components/Header/Header"
+import Bike from "./Components/Bike/Bike"
 
 const App = () => {
     return (
         <div className="App">
             <Header />
+            <Bike />
         </div>
     )
 };
---
+++ src/Components/Bike/Bike.js
@@ -0,0 +1,65 @@
+import React, { useRef } from "react";
+import "./Bike.css";
+import { Canvas, extend, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { useProgress, Html } from "@react-three/drei";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { Suspense } from "react";
+
+const Scene = () => {
+    const fbx = useLoader(FBXLoader, "Bike.fbx");
+
+    return <primitive object={fbx} scale={0.02} />;
+};
+
+extend({ OrbitControls })
+const Controls = () => {
+    //Configure camera and controls (see orbitControls docs for more info)
+    const controls = useRef();
+    const { camera, gl } = useThree();
+    //Set the camera position and the camera target
+    camera.position.set(0, 1, 4);
+    useFrame(() => controls.current.update());
+    return <orbitControls
+        ref={controls}
+        args={[camera, gl.domElement]}
+        enableDamping
+        enablePan={false}
+        enableZoom={false}
+        rotateSpeed={0.6}
+        minPolarAngle={1}
+        maxPolarAngle={1}
+        autoRotate={true}
+        autoRotateSpeed={0.75} />
+}
+
+const Loader = () => {
+    const { progress } = useProgress();
+    return (
+        <Html center>{progress} % loaded</Html>
+    );
+}
+
+const Lights = () => {
+    return (
+        <>
+            <ambientLight intensity={0.5} />
+        </>
+    );
+}
+
+const Bike = () => {
+    return (
+        <div className="bike">
+            <Canvas>
+                <Suspense fallback={<Loader />}>
+                    <Scene />
+                    <Lights />
+                </Suspense>
+                <Controls />
+            </Canvas>
+        </div>
+    );
+}
+
+export default Bike;
